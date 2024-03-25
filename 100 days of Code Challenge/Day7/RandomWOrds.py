0aaa5ed936d5e1bc04c1a29a9b7e6d0780c206a1
hakinz0110 <141330876+hakinz0110@users.noreply.github.com>
2024-03-25 21:30:30 +0100

    new

--- 100 days of Code Challenge/Day7/RandomWOrds.py
+++ 100 days of Code Challenge/Day7/RandomWOrds.py
@@ -1,23 +1,39 @@
 import random
   
-word_list = ["hello", "welcom", "museum", "rain", "engine", "skin", "before", "odor", "visa", "rebel", "shove", "female", "alpha", "dwarf"]
-
+lives = 6
+end_of_game = False
+word_list = ["hell", "welcome", "mum", "to", "engine", "force", "momentum"]
 chosen_word = random.choice(word_list)
+word_lenght = len(chosen_word)
 
+#create blanks
 display = []
 for _ in chosen_word:   ## for _ in range(len(chosen_word)):
-  display += "_"
-print(display)
+  isplay += "_"
 
-guess = input("Guess a letter: ").lower()
-word_lenght = len(chosen_word
-for position in (word_lenght)):
-  letter = chosen_word[position]
-  if letter == guess:
-    display[position] = letter
+
+while not end_of_game:  
+  guess = input("Guess a letter: ").lower()
+
+  #Check geussed leter
+  for position in range (word_lenght):
+    letter = chosen_word[position]
+    if letter == guess:
+      display[position] = letter
+    
+  print(display)
+  
+  if "_" not in display:
+    end_of_game = True
+    print("You Win!!!.")
+    
+    
 # for letter in chosen_word:
 #   if letter == guess:
 #     print("Right")
 #   else:
 #     print("Wrong")
 # print(chosen_word)
+
+
+ 
